94a65bc6c5903346170457b405af6873cf227f96
KhuatCong <khuatphucong06092002@gmail.com>
2023-06-27 12:27:43 +0700

    Add

---
+++ CJAMM_Movie/Home.Designer.cs
@@ -0,0 +1,199 @@
+﻿
+namespace CJAMM_Movie
+{
+    partial class Home
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picNext = new System.Windows.Forms.PictureBox();
+            this.picPre = new System.Windows.Forms.PictureBox();
+            this.picList = new System.Windows.Forms.PictureBox();
+            this.lblRes = new System.Windows.Forms.Label();
+            this.lblMovie = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblLogin = new System.Windows.Forms.Label();
+            this.lblSignUp = new System.Windows.Forms.Label();
+            this.lblMyCjamm = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picNext)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picPre)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picList)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picNext
+            // 
+            this.picNext.Image = global::CJAMM_Movie.Properties.Resources.Pasted_20230623_235306_pixian;
+            this.picNext.Location = new System.Drawing.Point(576, 311);
+            this.picNext.Name = "picNext";
+            this.picNext.Size = new System.Drawing.Size(68, 63);
+            this.picNext.TabIndex = 26;
+            this.picNext.TabStop = false;
+            // 
+            // picPre
+            // 
+            this.picPre.Image = global::CJAMM_Movie.Properties.Resources.Pasted_20230623_235239_pixian;
+            this.picPre.Location = new System.Drawing.Point(48, 311);
+            this.picPre.Name = "picPre";
+            this.picPre.Size = new System.Drawing.Size(68, 63);
+            this.picPre.TabIndex = 25;
+            this.picPre.TabStop = false;
+            // 
+            // picList
+            // 
+            this.picList.Image = global::CJAMM_Movie.Properties.Resources.BADGIRL;
+            this.picList.Location = new System.Drawing.Point(193, 151);
+            this.picList.Name = "picList";
+            this.picList.Size = new System.Drawing.Size(302, 407);
+            this.picList.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picList.TabIndex = 24;
+            this.picList.TabStop = false;
+            // 
+            // lblRes
+            // 
+            this.lblRes.AutoSize = true;
+            this.lblRes.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRes.ForeColor = System.Drawing.Color.SandyBrown;
+            this.lblRes.Location = new System.Drawing.Point(429, 77);
+            this.lblRes.Name = "lblRes";
+            this.lblRes.Size = new System.Drawing.Size(137, 24);
+            this.lblRes.TabIndex = 23;
+            this.lblRes.Text = "RESERVATION";
+            // 
+            // lblMovie
+            // 
+            this.lblMovie.AutoSize = true;
+            this.lblMovie.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMovie.ForeColor = System.Drawing.Color.SandyBrown;
+            this.lblMovie.Location = new System.Drawing.Point(597, 77);
+            this.lblMovie.Name = "lblMovie";
+            this.lblMovie.Size = new System.Drawing.Size(80, 24);
+            this.lblMovie.TabIndex = 22;
+            this.lblMovie.Text = "MOVIES";
+            // 
+            // label2
+            // 
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(0, 123);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(700, 2);
+            this.label2.TabIndex = 20;
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(0, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(700, 2);
+            this.label1.TabIndex = 19;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::CJAMM_Movie.Properties.Resources.Logo;
+            this.pictureBox1.Location = new System.Drawing.Point(20, 66);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(185, 45);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 18;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lblLogin
+            // 
+            this.lblLogin.AutoSize = true;
+            this.lblLogin.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLogin.Location = new System.Drawing.Point(449, 19);
+            this.lblLogin.Name = "lblLogin";
+            this.lblLogin.Size = new System.Drawing.Size(49, 19);
+            this.lblLogin.TabIndex = 27;
+            this.lblLogin.Text = "Login";
+            // 
+            // lblSignUp
+            // 
+            this.lblSignUp.AutoSize = true;
+            this.lblSignUp.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSignUp.Location = new System.Drawing.Point(513, 19);
+            this.lblSignUp.Name = "lblSignUp";
+            this.lblSignUp.Size = new System.Drawing.Size(61, 19);
+            this.lblSignUp.TabIndex = 28;
+            this.lblSignUp.Text = "Sign-up";
+            // 
+            // lblMyCjamm
+            // 
+            this.lblMyCjamm.AutoSize = true;
+            this.lblMyCjamm.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMyCjamm.Location = new System.Drawing.Point(585, 19);
+            this.lblMyCjamm.Name = "lblMyCjamm";
+            this.lblMyCjamm.Size = new System.Drawing.Size(99, 19);
+            this.lblMyCjamm.TabIndex = 29;
+            this.lblMyCjamm.Text = " My CJAMM";
+            // 
+            // Home
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(696, 587);
+            this.Controls.Add(this.lblMyCjamm);
+            this.Controls.Add(this.lblSignUp);
+            this.Controls.Add(this.lblLogin);
+            this.Controls.Add(this.picNext);
+            this.Controls.Add(this.picPre);
+            this.Controls.Add(this.picList);
+            this.Controls.Add(this.lblRes);
+            this.Controls.Add(this.lblMovie);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Home";
+            this.Text = "Home";
+            ((System.ComponentModel.ISupportInitialize)(this.picNext)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picPre)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picList)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picNext;
+        private System.Windows.Forms.PictureBox picPre;
+        private System.Windows.Forms.PictureBox picList;
+        private System.Windows.Forms.Label lblRes;
+        private System.Windows.Forms.Label lblMovie;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblLogin;
+        private System.Windows.Forms.Label lblSignUp;
+        private System.Windows.Forms.Label lblMyCjamm;
+    }
+}
+
---
+++ CJAMM_Movie/Login.Designer.cs
@@ -0,0 +1,234 @@
+﻿
+namespace CJAMM_Movie
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblMyCjamm = new System.Windows.Forms.Label();
+            this.lblSignUp = new System.Windows.Forms.Label();
+            this.lblHome = new System.Windows.Forms.Label();
+            this.lblRes = new System.Windows.Forms.Label();
+            this.lblMovie = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblMyCjamm
+            // 
+            this.lblMyCjamm.AutoSize = true;
+            this.lblMyCjamm.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMyCjamm.Location = new System.Drawing.Point(583, 15);
+            this.lblMyCjamm.Name = "lblMyCjamm";
+            this.lblMyCjamm.Size = new System.Drawing.Size(99, 19);
+            this.lblMyCjamm.TabIndex = 37;
+            this.lblMyCjamm.Text = " My CJAMM";
+            // 
+            // lblSignUp
+            // 
+            this.lblSignUp.AutoSize = true;
+            this.lblSignUp.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSignUp.Location = new System.Drawing.Point(511, 15);
+            this.lblSignUp.Name = "lblSignUp";
+            this.lblSignUp.Size = new System.Drawing.Size(61, 19);
+            this.lblSignUp.TabIndex = 36;
+            this.lblSignUp.Text = "Sign-up";
+            // 
+            // lblHome
+            // 
+            this.lblHome.AutoSize = true;
+            this.lblHome.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHome.Location = new System.Drawing.Point(447, 15);
+            this.lblHome.Name = "lblHome";
+            this.lblHome.Size = new System.Drawing.Size(49, 19);
+            this.lblHome.TabIndex = 35;
+            this.lblHome.Text = "Login";
+            // 
+            // lblRes
+            // 
+            this.lblRes.AutoSize = true;
+            this.lblRes.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRes.ForeColor = System.Drawing.Color.SandyBrown;
+            this.lblRes.Location = new System.Drawing.Point(427, 73);
+            this.lblRes.Name = "lblRes";
+            this.lblRes.Size = new System.Drawing.Size(137, 24);
+            this.lblRes.TabIndex = 34;
+            this.lblRes.Text = "RESERVATION";
+            // 
+            // lblMovie
+            // 
+            this.lblMovie.AutoSize = true;
+            this.lblMovie.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMovie.ForeColor = System.Drawing.Color.SandyBrown;
+            this.lblMovie.Location = new System.Drawing.Point(595, 73);
+            this.lblMovie.Name = "lblMovie";
+            this.lblMovie.Size = new System.Drawing.Size(80, 24);
+            this.lblMovie.TabIndex = 33;
+            this.lblMovie.Text = "MOVIES";
+            // 
+            // label2
+            // 
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(-2, 119);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(700, 2);
+            this.label2.TabIndex = 32;
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(-2, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(700, 2);
+            this.label1.TabIndex = 31;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::CJAMM_Movie.Properties.Resources.Logo;
+            this.pictureBox1.Location = new System.Drawing.Point(18, 62);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(185, 45);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 30;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.AutoSize = true;
+            this.btnLogin.BackColor = System.Drawing.Color.Lime;
+            this.btnLogin.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnLogin.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogin.Location = new System.Drawing.Point(228, 580);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(247, 34);
+            this.btnLogin.TabIndex = 43;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = false;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPassword.Location = new System.Drawing.Point(228, 485);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(247, 30);
+            this.txtPassword.TabIndex = 42;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(289, 460);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(114, 22);
+            this.label6.TabIndex = 41;
+            this.label6.Text = "PASSWORD";
+            // 
+            // txtID
+            // 
+            this.txtID.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtID.Location = new System.Drawing.Point(228, 377);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(247, 30);
+            this.txtID.TabIndex = 40;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(336, 352);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(30, 22);
+            this.label7.TabIndex = 39;
+            this.label7.Text = "ID";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::CJAMM_Movie.Properties.Resources._lock;
+            this.pictureBox2.Location = new System.Drawing.Point(289, 171);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(123, 132);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 38;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(696, 686);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.lblMyCjamm);
+            this.Controls.Add(this.lblSignUp);
+            this.Controls.Add(this.lblHome);
+            this.Controls.Add(this.lblRes);
+            this.Controls.Add(this.lblMovie);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Login";
+            this.Text = "Login";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblMyCjamm;
+        private System.Windows.Forms.Label lblSignUp;
+        private System.Windows.Forms.Label lblHome;
+        private System.Windows.Forms.Label lblRes;
+        private System.Windows.Forms.Label lblMovie;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
